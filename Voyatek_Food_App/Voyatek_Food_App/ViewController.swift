0513f067545bc0a62588d85e2a8561d735e3135c
devNasm <naseemoyebola@gmail.com>
2025-02-14 11:27:47 +0100

    Project initialization

---
+++ Voyatek_Food_App/Voyatek_Food_App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Voyatek_Food_App
+//
+//  Created by Naseem Oyebola on 14/02/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
---
+++ Voyatek_Food_App/Voyatek_Food_AppTests/Voyatek_Food_AppTests.swift
@@ -0,0 +1,17 @@
+//
+//  Voyatek_Food_AppTests.swift
+//  Voyatek_Food_AppTests
+//
+//  Created by Naseem Oyebola on 14/02/2025.
+//
+
+import Testing
+@testable import Voyatek_Food_App
+
+struct Voyatek_Food_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
